5333e32540fdbbb49758d043bcd2ec3ac529c411
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-06-02 14:20:17 +0300

    basic layout + mock data

---
+++ src/components/columns.js
@@ -0,0 +1,31 @@
+// labels for each of columns
+// ! mock data columns: email & age are intentionally ignored
+export const COLUMNS = [
+  {
+    Header: 'Id',
+    // map each column to a specific data value in the mock data.
+    // Help react table identify what data goes under which column
+    // in each row
+    accessor: 'ind',
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name',
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'date_of_birth',
+  },
+  {
+    Header: 'Country',
+    accessor: 'country',
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+  },
+];
